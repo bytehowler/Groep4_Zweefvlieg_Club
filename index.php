9dc42cd2f3698f4c73a2ef69f1e86a6109c86567
Luke Idenburg <28677981+AlfaLynx@users.noreply.github.com>
2023-06-08 14:25:53 +0200

    Navbar update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Sky High</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -22,10 +22,10 @@
                     <a class="nav-link" href="vluchtplanner.html">Vluchtplanner</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Inloggen</a>
+                    <a class="nav-link" href="signin.php">Inloggen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Registreren</a>
+                    <a class="nav-link" href="signup.php">Registreren</a>
                 </li>
             </ul>
         </div>
