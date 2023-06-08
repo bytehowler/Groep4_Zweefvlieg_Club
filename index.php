8c5c3e722c513b01198e8340edbbaefcafc88344
Luke Idenburg <28677981+AlfaLynx@users.noreply.github.com>
2023-06-08 14:31:26 +0200

    Header made in to seperate file

--- index.php
+++ index.php
@@ -10,27 +10,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Sky High</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="vluchtplanner.html">Vluchtplanner</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="signin.php">Inloggen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="signup.php">Registreren</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<?php require "header.php"; ?>
 
 <main>
     <h2>Welkom bij Zweefvliegclub sky high! </h2>
