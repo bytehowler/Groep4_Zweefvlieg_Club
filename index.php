cc11be36cb6d38ceeaf2750ebca7937eb401be7b
Steph Bootsman <118430743+stephbootsman@users.noreply.github.com>
2023-06-08 12:53:29 +0200

    Added navbar

--- index.php
+++ index.php
@@ -3,10 +3,37 @@
 <head>
     <title>Mijn Homepagina</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <header>
-    <h1>Sky High</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Mijn Homepagina</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="vluchtplanner.html">Vluchtplanner</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inloggen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registreren</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
 
 <main>
