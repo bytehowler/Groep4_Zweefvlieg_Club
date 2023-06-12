486185618eb722f6b79f6af15442a7362ab92c53
RusbachSem <116639875+RusbachSem@users.noreply.github.com>
2023-06-12 07:59:22 +0200

    Add files via upload
    Added files from main branch

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mijn Homepagina</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+<header>
+    <h1>Mijn Homepagina</h1>
+</header>
+
+<main>
+    <h2>Welkom bij Zweefvliegclub sky high! </h2>
+    <p>Dit is een voorbeeld van een homepagina gemaakt met HTML en CSS.</p>
+</main>
+
+<footer>
+    &copy; 2023 Mijn Homepagina. Alle rechten voorbehouden.
+</footer>
+</body>
+</html>
